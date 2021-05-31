62ceeea2f3359092cde2ad0fdb3e693b87268ba0
Jeremy <jeremyeparker@yahoo.com>
2021-05-31 15:15:07 -0700

    first commit

---
+++ src/test/java/com/atsignJar/SpringPostgresDemo/SpringPostgresDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsignJar.SpringPostgresDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
